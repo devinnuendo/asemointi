b8146528cca4274f9e3ad379da8afb2d95963557
Jenniina <jenniina@jenniina.fi>
2023-09-03 15:03:09 +0300

    fix footer

--- sivuosat/footer.php
+++ sivuosat/footer.php
@@ -1,40 +1,36 @@
 <footer>
     <address>
-        <section>
-            <div>
-                Puutarhaliike Neilikka, Helsinki <br />
-                Fabianinkatu 42 <br />
-                00100 Helsinki <br />
-                Puh. <a href="tel:xx-xxxxxxx">xx-xxxxxxx</a> <br />
-                Sähköposti: <a href="mailto:helsinki@puutarhaliikeneilikka.fi">helsinki@puutarhaliikeneilikka.fi</a>
+        <div>
+            Puutarhaliike Neilikka, Helsinki <br />
+            Fabianinkatu 42 <br />
+            00100 Helsinki <br />
+            Puh. <a href="tel:xx-xxxxxxx">xx-xxxxxxx</a> <br />
+            Sähköposti: <a href="mailto:helsinki@puutarhaliikeneilikka.fi">helsinki@puutarhaliikeneilikka.fi</a>
 
-                <p>
-                    Avoinna<br />
-                    ma-ke 9-17<br />
-                    la 12-18
-                </p>
-            </div>
-        </section>
-        <section>
-            <div>
-                Puutarhaliike Neilikka, Espoo <br />
-                Kivenlahdentie 10 <br />
-                01234 Espoo <br />
-                Puh. <a href="tel:xx-xxxxxxx">xx-xxxxxxx</a> <br />
-                Sähköposti: <a href="mailto:espoo@puutarhaliikeneilikka.fi">espoo@puutarhaliikeneilikka.fi</a>
+            <p>
+                Avoinna<br />
+                ma-ke 9-17<br />
+                la 12-18
+            </p>
+        </div>
+        <div>
+            Puutarhaliike Neilikka, Espoo <br />
+            Kivenlahdentie 10 <br />
+            01234 Espoo <br />
+            Puh. <a href="tel:xx-xxxxxxx">xx-xxxxxxx</a> <br />
+            Sähköposti: <a href="mailto:espoo@puutarhaliikeneilikka.fi">espoo@puutarhaliikeneilikka.fi</a>
 
-                <p>Avoinna<br />
-                    ma-ke 9-17<br />
-                    la 12-18
-                </p>
-            </div>
-        </section>
+            <p>Avoinna<br />
+                ma-ke 9-17<br />
+                la 12-18
+            </p>
+        </div>
     </address>
     <small>
         <a href="mailto:asiakaspalvelu@puutarhaliikeneilikka.fi">asiakaspalvelu@puutarhaliikeneilikka.fi</a>
-        <span class="sep">|</span>
+        <small class="sep" aria-hidden="true">🙐</small>
         <span>&copy;<?php echo date("Y"); ?> Jenniina Laine</span>
-        <span class="sep">|</span>
+        <small class="sep" aria-hidden="true">🙖</small>
         <span>Kuvat Pixabay ja Flaticon</span>
     </small>
 
