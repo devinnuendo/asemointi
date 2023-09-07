e05c2635cc978474f184a02dc591b0a1b8cc62fb
Jenniina <jenniina@jenniina.fi>
2023-09-07 12:25:05 +0300

    add env call

--- api2/fetch.php
+++ api2/fetch.php
@@ -1,8 +1,9 @@
 <?php
 define('directAccess', TRUE);
 include 'scrt.php';
+$key = KEY ? KEY : getenv("KEY");
 
-$url = 'http://data.fixer.io/api/latest?access_key=' . KEY . '&base=EUR';
+$url = 'http://data.fixer.io/api/latest?access_key=' . $key . '&base=EUR';
 
 $response = file_get_contents($url);
 echo $response;
