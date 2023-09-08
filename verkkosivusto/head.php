38a8f7a1da2201d3518771aca80870dfd60c5a7e
Jenniina <jenniina@jenniina.fi>
2023-09-08 14:29:22 +0300

    add bootstrap

--- verkkosivusto/head.php
+++ verkkosivusto/head.php
@@ -5,10 +5,12 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="icon" type="image/x-icon" href="https://jenniina.fi/wp-content/uploads/2022/09/favicon.ico" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" />
   <link rel="stylesheet" type="text/css" href="css/styles-nav.css" />
   <link rel="stylesheet" type="text/css" href="css/styles.css" />
-  <script src="scripts/scripts.js"></script>
+  <script defer src="scripts/scripts.js"></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <?php if (isset($css)) echo "<link rel='stylesheet' href='$css'>"; ?>
   <title><?= $title ?? 'Sivusto'; ?></title>
 </head>
--- verkkosivusto/lomake.php
+++ verkkosivusto/lomake.php
@@ -7,76 +7,117 @@ $css = 'css/styles-lomake.css';
 
 <main>
   <section>
-    <form>
-      <fieldset>
-        <legend>Yhteystiedot</legend>
-        <label for="nimi">Nimi</label>
-        <input type="text" name="nimi" id="nimi" placeholder="Nimi" required />
+    <div class="container mt-5">
+      <h2 class="mb-4">Yhteystiedot</h2>
+      <form action="#" method="POST">
+        <fieldset>
+          <legend>Yhteystiedot</legend>
+          <div class="mb-3">
+            <label for="nimi" class="form-label">Nimi</label>
+            <input type="text" name="nimi" id="nimi" class="form-control" placeholder="Nimi" required />
+          </div>
 
-        <label for="katuosoite">Katuosoite</label>
-        <input type="text" name="katuosoite" id="katuosoite" placeholder="Katuosoite" />
+          <div class="mb-3">
+            <label for="katuosoite" class="form-label">Katuosoite</label>
+            <input type="text" name="katuosoite" id="katuosoite" class="form-control" placeholder="Katuosoite" />
+          </div>
 
-        <label for="postinumero">Postinumero</label>
-        <input type="number" name="postinumero" id="postinumero" placeholder="Postinumero" />
+          <div class="mb-3">
+            <label for="postinumero" class="form-label">Postinumero</label>
+            <input type="number" name="postinumero" id="postinumero" class="form-control" placeholder="Postinumero" />
+          </div>
 
-        <label for="postitoimipaikka">Postitoimipaikka</label>
-        <input type="text" name="postitoimipaikka" id="postitoimipaikka" placeholder="Postitoimipaikka" />
+          <div class="mb-3">
+            <label for="postitoimipaikka" class="form-label">Postitoimipaikka</label>
+            <input type="text" name="postitoimipaikka" id="postitoimipaikka" class="form-control" placeholder="Postitoimipaikka" />
+          </div>
 
-        <label for="puhelinnumero">Puhelinnumero</label>
-        <input type="tel" name="puhelinnumero" id="puhelinnumero" placeholder="Puhelinnumero" pattern="^[\d]{7,15}$" required />
+          <div class="mb-3">
+            <label for="puhelinnumero" class="form-label">Puhelinnumero</label>
+            <input type="tel" name="puhelinnumero" id="puhelinnumero" class="form-control" placeholder="Puhelinnumero" pattern="^[\d]{7,15}$" required />
+          </div>
 
-        <label for="sahkopostiosoite">Sähköpostiosoite</label>
-        <input type="email" name="sahkopostiosoite" id="sahkopostiosoite" placeholder="Sähköpostiosoite" minlength="12" required pattern="^[A-Za-z0-9+_.-]+@(.+)\.[A-Za-z0-9]{2,}$" />
+          <div class="mb-3">
+            <label for="sahkopostiosoite" class="form-label">Sähköpostiosoite</label>
+            <input type="email" name="sahkopostiosoite" id="sahkopostiosoite" class="form-control" placeholder="Sähköpostiosoite" minlength="12" required />
+          </div>
 
-        <label for="salasana">Salasana</label>
-        <input type="password" name="salasana" id="salasana" placeholder="Salasana" required />
-      </fieldset>
+          <div class="mb-3">
+            <label for="salasana" class="form-label">Salasana</label>
+            <input type="password" name="salasana" id="salasana" class="form-control" placeholder="Salasana" required />
+          </div>
+        </fieldset>
 
-      <fieldset class="osastot">
-        <legend>Mistä osastoista olet kiinnostunut?</legend>
+        <fieldset class="osastot">
+          <legend>Mistä osastoista olet kiinnostunut?</legend>
+          <div class="form-check">
+            <input type="checkbox" name="osasto" value="Muoti" class="form-check-input" id="osasto-muoti" />
+            <label class="form-check-label" for="osasto-muoti">Muoti</label>
+          </div>
+          <div class="form-check">
+            <input type="checkbox" name="osasto" value="Urheilu" class="form-check-input" id="osasto-urheilu" />
+            <label class="form-check-label" for="osasto-urheilu">Urheilu</label>
+          </div>
+          <div class="form-check">
+            <input type="checkbox" name="osasto" value="Sisustaminen" class="form-check-input" id="osasto-sisustaminen" />
+            <label class="form-check-label" for="osasto-sisustaminen">Sisustaminen</label>
+          </div>
+          <div class="form-check">
+            <input type="checkbox" name="osasto" value="Pelit" class="form-check-input" id="osasto-pelit" />
+            <label class="form-check-label" for="osasto-pelit">Pelit</label>
+          </div>
+          <div class="form-check">
+            <input type="checkbox" name="osasto" value="Elokuvat" class="form-check-input" id="osasto-elokuvat" />
+            <label class="form-check-label" for="osasto-elokuvat">Elokuvat</label>
+          </div>
+        </fieldset>
 
-        <label><input type="checkbox" name="osasto" value="Muoti" />Muoti</label>
+        <fieldset>
+          <legend>Maksutapa</legend>
+          <div class="mb-3">
+            <label for="maksutapa" class="form-label">Valitse maksutapa</label>
+            <select name="maksutapa" id="maksutapa" class="form-select">
+              <option value="empty">Valitse</option>
+              <option value="sampo">Sampo</option>
+              <option value="nordea">Nordea</option>
+              <option value="osuuspankki">Osuuspankki</option>
+              <option value="aktia">Aktia</option>
+            </select>
+          </div>
+        </fieldset>
 
-        <label><input type="checkbox" name="osasto" value="Urheilu" />Urheilu</label>
+        <fieldset>
+          <legend>Anna palautetta</legend>
+          <div class="mb-3">
+            <label for="palautetta" class="form-label">Kirjoita palautetta</label>
+            <textarea name="palautetta" id="palautetta" rows="4" class="form-control" placeholder="Kirjoita palautetta"></textarea>
+          </div>
+        </fieldset>
 
-        <label><input type="checkbox" name="osasto" value="Sisustaminen" />Sisustaminen</label>
+        <fieldset>
+          <legend>Haluan tilata uutiskirjeen</legend>
+          <div class="form-check">
+            <input type="radio" name="uutiskirje" id="kylla" value="Kylla" class="form-check-input" />
+            <label class="form-check-label" for="kylla">Kyllä</label>
+          </div>
+          <div class="form-check">
+            <input type="radio" name="uutiskirje" id="ei" value="Ei" class="form-check-input" />
+            <label class="form-check-label" for="ei">Ei</label>
+          </div>
+        </fieldset>
 
-        <label><input type="checkbox" name="osasto" value="Pelit" />Pelit</label>
+        <fieldset>
+          <legend>Hyväksyn tuotteiden toimitusehdot</legend>
+          <div class="form-check">
+            <input type="checkbox" name="toimitusehdot" id="ok" value="ok" class="form-check-input" required />
+            <label class="form-check-label" for="ok">Hyväksyn toimitusehdot</label>
+          </div>
+        </fieldset>
 
-        <label><input type="checkbox" name="osasto" value="Elokuvat" />Elokuvat</label>
-      </fieldset>
+        <button type="submit" class="btn btn-primary">Lähetä</button>
+      </form>
+    </div>
 
-      <fieldset>
-        <legend><label for="maksutapa">Maksutapa</label></legend>
-        <select name="maksutapa" id="maksutapa">
-          <option value="empty">Valitse</option>
-          <option value="sampo">Sampo</option>
-          <option value="nordea">Nordea</option>
-          <option value="osuuspankki">Osuuspankki</option>
-          <option value="aktia">Aktia</option>
-        </select>
-      </fieldset>
-      <fieldset>
-        <legend><label for="palautetta">Anna palautetta</label></legend>
-        <textarea name="palautetta" id="palautetta" rows="4" placeholder="Kirjoita palautetta"></textarea>
-      </fieldset>
-
-      <fieldset>
-        <legend>Haluan tilata uutiskirjeen</legend>
-        <input type="radio" name="uutiskirje" id="kylla" value="Kylla" />
-        <label for="kylla">Kyllä</label>
-        <input type="radio" name="uutiskirje" id="ei" value="Ei" />
-        <label for="ei">Ei</label>
-      </fieldset>
-
-      <fieldset>
-        <legend>Olen lukenut ja hyväksyn tuotteiden toimitusehdot</legend>
-        <input type="checkbox" name="toimitusehdot" id="ok" value="ok" required />
-        <label for="ok">Kyllä</label>
-      </fieldset>
-
-      <button type="submit">Lähetä</button>
-    </form>
   </section>
 </main>
 <?php include "footer.php"; ?>
