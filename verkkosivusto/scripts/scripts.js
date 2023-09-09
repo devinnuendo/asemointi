480ea153698198c4d399a740fdf4edc0a2ec48f8
Jenniina <jenniina@jenniina.fi>
2023-09-09 15:28:42 +0300

    add menu closing on outside click

--- verkkosivusto/scripts/scripts.js
+++ verkkosivusto/scripts/scripts.js
@@ -6,3 +6,10 @@ window.addEventListener('resize', function (event) {
   /* close menu on resize */
   document.querySelector('#main-nav').className = 'main-nav'
 })
+
+window.addEventListener('click', function (e) {
+  /* close menu on outside menu click */
+  if (!document.querySelector('#main-nav').contains(e.target)) {
+    document.querySelector('#main-nav').className = 'main-nav'
+  }
+})
