8f8f09765b820bcbf76c56a6300ce4677601e210
Jenniina <jenniina@jenniina.fi>
2023-09-10 07:40:25 +0300

    add links to files

--- verkkosivusto/scroll.php
+++ verkkosivusto/scroll.php
@@ -7,6 +7,13 @@ include "head.php";
 <main>
     <section>
         <p>Ns. "infinite scroll." Scroll.js hakee fetch-komennolla posts.php-tiedostosta randomoitua lorem ipsum -materiaalia ja sijoittaa sen artikkeleihin. </p>
+        <h2>Olennaiset tiedostot</h2>
+        <ul>
+            <li><a href="https://github.com/devinnuendo/asemointi/blob/main/verkkosivusto/scroll.php">scroll.php</a></li>
+            <li><a href="https://github.com/devinnuendo/asemointi/blob/main/verkkosivusto/posts.php">posts.php</a></li>
+            <li><a href="https://github.com/devinnuendo/asemointi/blob/main/verkkosivusto/scripts/scroll.js">scroll.js</a></li>
+            <li><a href="https://github.com/devinnuendo/asemointi/blob/main/verkkosivusto/css/styles-scroll.css">styles-scroll.css</a></li>
+        </ul>
     </section>
     <section id="posts" class="posts"></section>
 </main>
