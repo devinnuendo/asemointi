c6e9b955e207951c7d702e508bb4bc25029343e6
Jenniina <jenniina@jenniina.fi>
2023-09-18 13:20:17 +0300

    add sample page

---
+++ neilikka/sivupohja.php
@@ -0,0 +1,14 @@
+<?php
+$title = 'Otsikko';
+// $img640 = 'img/flower-3231083_640.jpg';
+// $img1280 = 'img/flower-3231083_1280.jpg';
+// $img1920 = 'img/flower-3231083_1920.jpg';
+include "sivuosat/header.php"; ?>
+
+<main class="etusivu">
+    <section>
+
+    </section>
+</main>
+
+<?php include "sivuosat/footer.php"; ?>
