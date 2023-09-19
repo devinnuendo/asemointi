f55ad12248bd056e71df417165ed1cd3295a8c73
Jenniina <jenniina@jenniina.fi>
2023-09-19 08:12:19 +0300

    edit title

--- neilikka/kirjaudu-kasittely.php
+++ neilikka/kirjaudu-kasittely.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Kiitos kirjautumisesta';
+$title = 'Kirjautuminen';
 // $img640 = 'flower-3231083_640.jpg';
 // $img1280 = 'flower-3231083_1280.jpg';
 // $img1920 = 'flower-3231083_1920.jpg';
