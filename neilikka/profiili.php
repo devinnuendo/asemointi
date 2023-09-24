69b2b38661dba3ad0943ccf8b0c9cdad28fed9fe
Jenniina <jenniina@jenniina.fi>
2023-09-24 17:44:27 +0300

    add secure_page()

--- neilikka/profiili.php
+++ neilikka/profiili.php
@@ -4,7 +4,9 @@ $title = $traCommon['profile'][$lang];
 // $img640 = 'flower-3231083_640.jpg';
 // $img1280 = 'flower-3231083_1280.jpg';
 // $img1920 = 'flower-3231083_1920.jpg';
-include "sivuosat/header.php"; ?>
+$loggedIn = secure_page();
+include "sivuosat/header.php";
+?>
 
 <main>
     <section>
