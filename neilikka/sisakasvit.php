c0a42369f4a66b0ee140fa1882880771a88d29db
Jenniina <jenniina@jenniina.fi>
2023-09-26 08:32:38 +0300

    add database select

--- neilikka/sisakasvit.php
+++ neilikka/sisakasvit.php
@@ -1,110 +1,176 @@
 <?php
 include "sivuosat/top.php";
 $title = $traLocal['plants_indoor'][$lang];
-include "sivuosat/header.php"; ?>
+include "sivuosat/header.php";
+
+$QUERY_STRING = $_SERVER['QUERY_STRING'];
+$pageosa = strpos($QUERY_STRING, "&page=");
+if ($pageosa !== false) {
+    $search_parameters = substr($QUERY_STRING, 0, $pageosa);
+} else
+    $search_parameters = $QUERY_STRING;
+
+$row_count = 8;
+$right_disabled = "";
+$left_disabled = "";
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$offset = ($page - 1) * $row_count;
+$prev = ($page - 1) ?: 1;
+$next = $page + 1;
+$query = "SELECT COUNT(*) AS rivit FROM neil_plants_fi WHERE habitat = 'indoor'";
+$results_initial = $yhteys->query($query);
+$rows = $results_initial->fetch_assoc();
+$rows = $rows['rivit'];
+
+$last = ceil($rows / $row_count);
+$last = $last ?: 1;
+if ($page >= $last) {
+    $page = $last;
+    // $offset = ($page - 1) * $row_count;
+    $right_disabled = "disabled";
+}
+if ($page == 1) {
+    $left_disabled = "disabled";
+}
+
+$query_main =
+    $lang == 'fi'
+    ? ("SELECT 
+        id,
+        plant AS name,
+        sci,
+        description,
+        amount,
+        color,
+        price,
+        length,
+        habitat,
+        type,
+        img_small,
+        img_large
+        FROM
+        neil_plants_fi WHERE habitat = 'indoor'
+        LIMIT $offset, $row_count;
+        "
+    )
+    : ($lang == 'sv'
+        ? ("SELECT 
+                p.id AS id,
+                sv.plant AS name,
+                p.sci,
+                sv.description AS description,
+                p.amount,
+                sv.color AS color,
+                p.price,
+                p.length,
+                p.habitat,
+                p.type,
+                p.img_small,
+                p.img_large
+                FROM 
+                neil_plants_fi AS p
+                LEFT JOIN
+                neil_plants_sv AS sv ON p.id = sv.original_id
+                WHERE habitat = 'indoor'
+                LIMIT $offset, $row_count;
+                "
+        )
+        : ("SELECT
+                p.id AS id,
+                en.plant AS name,
+                p.sci,
+                en.description AS description,
+                p.amount,
+                en.color AS color,
+                p.price,
+                p.length,
+                p.habitat,
+                p.type,
+                p.img_small,
+                p.img_large
+                FROM
+                neil_plants_fi AS p 
+                LEFT JOIN
+                neil_plants_en AS en ON p.id = en.original_id
+                WHERE habitat = 'indoor'
+                LIMIT $offset, $row_count;
+                "
+        )
+    );
+$result_main = $yhteys->query($query_main);
+?>
 
 <main>
     <?php include "sivuosat/inner-nav.php"; ?>
     <section class="kauppa">
-        <ul>
-            <li>
-                <a href="#">
-                    <figure>
-                        <img src="img/roses-6887974_640.jpg" alt="punaisia ruusuja" />
-                        <figcaption>
-                            <em><?= $traLocal['flower_rose'][$lang] ?>, <?= strtolower($traCommon['color_red'][$lang]) ?></em>
-                            <small><?= $traLocal['plants_cut'][$lang] ?>, 70-80 cm, 1 <?= $traCommon['pieces'][$lang] ?></small>
-                            <strong>10 &euro;</strong>
-                        </figcaption>
-                    </figure>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <figure>
-                        <img src="img/rose-3337091_640.jpg" alt="valkoisia ruusuja" />
-                        <figcaption>
-                            <em><?= $traLocal['flower_rose'][$lang] ?>, <?= strtolower($traCommon['color_white'][$lang]) ?></em>
-                            <small><?= $traLocal['plants_cut'][$lang] ?>, 70-80 cm, 1 <?= $traCommon['pieces'][$lang] ?></small>
-                            <strong>10 &euro;</strong>
-                        </figcaption>
-                    </figure>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <figure>
-                        <img src="img/carnation-1325012_640.jpg" alt="neilikka" />
-                        <figcaption>
-                            <em><?= $traLocal['flower_carnation'][$lang] ?>, <?= strtolower($traCommon['color_pink'][$lang]) ?></em>
-                            <small><?= $traLocal['plants_cut'][$lang] ?>, 64 cm, 3 <?= $traCommon['pieces'][$lang] ?></small>
-                            <strong>8,90 &euro;</strong>
-                        </figcaption>
-                    </figure>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <figure>
-                        <img src="img/bunch-of-flowers-1018557_640.jpg" alt="kesäinen kukkakimppu" />
-                        <figcaption>
-                            <em><?= $traLocal['like_summer'][$lang] ?> &ndash; <?= $traLocal['flower_bunch'][$lang] ?></em>
-                            <small><?= $traLocal['flower_bunch'][$lang] ?>, <?= $traCommon['color_yellow'][$lang] ?> <?= strtolower($traLocal['flower_rose'][$lang]) ?></small>
-                            <strong>50 &euro;</strong>
-                        </figcaption>
-                    </figure>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <figure>
-                        <img src="img/bunch-of-flowers-3116227_640.jpg" alt="juhlava kukkakimppu" />
-                        <figcaption>
-                            <em><?= $traLocal['like_elegant'][$lang] ?> &ndash; <?= $traLocal['flower_bunch'][$lang] ?></em>
-                            <small><?= $traLocal['flower_rose'][$lang] ?>, <?= strtolower($traCommon['color_pink'][$lang]) ?></small>
-                            <strong>60 &euro;</strong>
-                        </figcaption>
-                    </figure>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <figure>
-                        <img src="img/hyacinth-1230667_640.jpg" alt="hyasintti" />
-                        <figcaption>
-                            <em><?= $traLocal['flower_hyasinth'][$lang] ?></em>
-                            <small><?= $traLocal['plants_pot'][$lang] ?></small>
-                            <strong>5,90 &euro;</strong>
-                        </figcaption>
-                    </figure>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <figure>
-                        <img src="img/gerbera-955803_640.jpg" alt="gerbera" />
-                        <figcaption>
-                            <em>Gerbera</em>
-                            <small><?= $traLocal['plants_pot'][$lang] ?></small>
-                            <strong>6.90 &euro;</strong>
-                        </figcaption>
-                    </figure>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <figure>
-                        <img src="img/flower-1829711_640.png" alt="joulutähti" />
-                        <figcaption>
-                            <em><?= $traLocal['flower_poinsettia'][$lang] ?></em>
-                            <small><?= $traLocal['plants_pot'][$lang] ?></small>
-                            <strong>10 &euro;</strong>
-                        </figcaption>
-                    </figure>
-                </a>
-            </li>
+        <?php pagination($search_parameters, $left_disabled, $right_disabled, $prev, $next, $last); ?>
+        <ul id="lista">
+            <?php
+            if ($result_main === false) {
+                die("Query error: " . $yhteys->error);
+            }
+
+            if ($result_main->num_rows > 0) {
+            ?>
+                <?php while ($row = $result_main->fetch_assoc()) { ?>
+                    <li class="<?= isset($_GET['id']) && $_GET['id'] == $row['id'] ? 'active' : '' ?>" id="<?= $row['name'] ?>-<?= $row['color'] ?>">
+                        <?php
+                        if (isset($_GET['id']) && $_GET['id'] == $row['id']) {
+                        ?>
+                            <div class="absolute right top close-btn">
+                                <a href="sisakasvit.php#lista" class="tooltip left below" data-tooltip="<?= $traCommon['close'][$lang]; ?>">
+                                    <span aria-hidden="true">&times;</span>
+                                    <span class="scr"><?= $traCommon['close'][$lang] ?></span>
+                                </a>
+                            </div>
+                        <?php
+                        };
+                        if (!isset($_GET['id'])) {
+                        ?> <a href="sisakasvit.php?id=<?= $row['id'] ?>#<?= $row['name'] ?>-<?= $row['color'] ?>"> <?php } ?>
+                            <figure>
+                                <?php
+                                if (isset($_GET['id']) && $_GET['id'] == $row['id']) {
+                                ?>
+                                    <img src="img/<?= $row['img_large'] ?>" alt="<?= $row['name'] ?>, <?= $row['color'] ?>" />
+                                <?php
+                                } else {
+                                ?>
+                                    <img src="img/<?= $row['img_small'] ?>" alt="<?= $row['name'] ?>, <?= $row['color'] ?>" />
+                                <?php }; ?>
+                                <figcaption>
+                                    <div class="item-general">
+                                        <em><?= $row['name'] ?>, <?= $row['color'] ?></em>
+                                        <small><?= $row['type'] ?>, <?= $traCommon['about'][$lang] ?> <?= $row['length'] ?> cm</small>
+                                        <strong><?= $row['price'] ?> &euro;</strong>
+                                    </div>
+                                    <?php
+                                    if (isset($_GET['id']) && $_GET['id'] == $row['id']) {
+                                    ?>
+                                        <div class="item-description">
+                                            <p><?= $row['description'] ?></p>
+                                            <form action="ostoskori.php?id=<?= $row['id'] ?>&amp;action=add" class="reset no-padding flex">
+                                                <!-- <a href="ostoskori.php?id=<?= $row['id'] ?>&amp;action=add"> </a>-->
+                                                <input type="number" name="item-amount" class="narrow" />
+                                                <button type="submit" name="add" class="add-to-cart" value="<?= $row['id'] ?>">
+                                                    <?= $traCommon['cart_add_to'][$lang] ?>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    <?php
+                                    }
+                                    ?>
+                                </figcaption>
+                            </figure>
+                            <?php
+                            if (!isset($_GET['id'])) {
+                            ?>
+                            </a> <?php }; ?>
+                    </li>
+                <?php }; ?>
+            <?php }; ?>
 
         </ul>
+        <?php pagination($search_parameters, $left_disabled, $right_disabled, $prev, $next, $last); ?>
     </section>
 </main>
 
