29de9f316dc8ac89736981c888b8a691f6936ed5
Jenniina <jenniina@jenniina.fi>
2023-09-28 07:37:22 +0300

    add new page

---
+++ neilikka/kayttajat.php
@@ -0,0 +1,17 @@
+<?php
+include "sivuosat/top.php";
+$title = $traCommon['users'][$lang];
+// $img640 = 'flower-3231083_640.jpg';
+// $img1280 = 'flower-3231083_1280.jpg';
+// $img1920 = 'flower-3231083_1920.jpg';
+$script = 'lomake.js';
+$css = 'styles-lomake.css';
+include "sivuosat/header.php"; ?>
+
+<main>
+    <section>
+
+    </section>
+</main>
+
+<?php include "sivuosat/footer.php"; ?>
