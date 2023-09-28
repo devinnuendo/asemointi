9b7dc2070d975e8518214469b3799def26d63545
Jenniina <jenniina@jenniina.fi>
2023-09-28 06:18:36 +0300

    redirect logged in users to index, remove commented code

--- neilikka/rekisteroidy.php
+++ neilikka/rekisteroidy.php
@@ -6,43 +6,8 @@ $css = 'styles-lomake.css';
 // $img640 = 'flower-3231083_640.jpg';
 // $img1280 = 'flower-3231083_1280.jpg';
 // $img1920 = 'flower-3231083_1920.jpg';
+$loggedIn ? header("Location: index.php") : NULL;
 include "sivuosat/header.php";
-
-
-// $errors = [
-//     'first_name' => [
-//         'puuttuu' => "Etunimi puuttuu",
-//         'lyhyt' => "Etunimi on liian lyhyt",
-//         'oikein' => "Anna etunimi oikein"
-//     ],
-//     'last_name' => [
-//         'puuttuu' => "Sukunimi puuttuu",
-//         'lyhyt' => "Sukunimi on liian lyhyt",
-//         'oikein' => "Anna sukunimi oikein"
-//     ],
-//     'email' => [
-//         'puuttuu' => "Sähköpostiosoite puuttuu",
-//         'oikein' => "Anna sähköpostiosoite oikein"
-//     ],
-//     'password' => [
-//         'puuttuu' => "Salasana puuttuu",
-//         'lyhyt' => "Salasana on liian lyhyt",
-//         'tasmaa' => "Salasanat eivät täsmää"
-//     ],
-//     'password2' => [
-//         'puuttuu' => "Salasana puuttuu",
-//         'lyhyt' => "Salasana on liian lyhyt",
-//         'tasmaa' => "Salasanat eivät täsmää"
-//     ],
-//     'terms' => [
-//         'puuttuu' => "Hyväksy käyttösäännöt"
-//     ]
-// ];
-
-// function error($field)
-// {
-//     return $GLOBALS['errors'][$field] ?? $GLOBALS['errors'][$field]['puuttuu'];
-// }
 ?>
 
 <main class="rekisteroidy">
